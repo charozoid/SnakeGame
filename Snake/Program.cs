43b703315f03e5c3fe80dd2db63fc6a1fe15b29a
charozoid <directfromquebec@gmail.com>
2023-12-22 11:46:31 -0500

    Added end state, score, end animation

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -2,19 +2,22 @@
 using SFML.System;
 using SFML.Window;
 using System.Diagnostics;
+using System.Xml.Linq;
 
 class SnakeGame
 {
     public const int WIDTH = 800;
     public const int HEIGHT = 608;
+    public const int BLOCK_SIZE = 32;
+    public const int GRID_SIZE_X = WIDTH / BLOCK_SIZE;
+    public const int GRID_SIZE_Y = HEIGHT / BLOCK_SIZE;
 
-    public const int GRID_SIZE_X = WIDTH / 16;
-    public const int GRID_SIZE_Y = HEIGHT / 16;
     public static ICollidable[,] grid = new ICollidable[GRID_SIZE_X, GRID_SIZE_Y];
+
     public static bool gameOver = false;
+    public static int score = 0;
     public static void Main(string[] args)
     {
-
         RenderWindow window = new RenderWindow(new SFML.Window.VideoMode(WIDTH, HEIGHT), "Snake");
 
         window.SetVerticalSyncEnabled(true);
@@ -24,26 +27,30 @@ class SnakeGame
         snake.GridPos = new Vector2f(1, 1);
         window.KeyPressed += (sender, e) => OnKeyPressed(sender, e, snake);
         Stopwatch stopwatch = new Stopwatch();
-
-        if (!gameOver)
-        {
-            stopwatch.Restart();
-        }
-
+        Stopwatch animationsStopwatch = new Stopwatch();
+        Text text = new Text("", new Font("arial.ttf"));
+        text.Position = new Vector2f(WIDTH / 2 - 150, HEIGHT / 2 - 50);
+        text.FillColor = Color.Red;
+        animationsStopwatch.Restart();
+        stopwatch.Restart();
         while (window.IsOpen)
         {
-            //OnKeyPressed(snake);
             window.DispatchEvents();
             window.Clear(Color.Black);
+            if (gameOver && animationsStopwatch.ElapsedMilliseconds > 50)
+            {
+                text.DisplayedString = "Game Over! Score: " + (score);
+                snake.RemoveBody();
+                snake.head.FillColor = Color.Red;
+                animationsStopwatch.Restart();
+            }
             if (stopwatch.ElapsedMilliseconds > 200)
             {
                 stopwatch.Restart();
                 snake.Move();
             }
-            foreach (Block block in snake.blocks)
-            {
-                window.Draw(block.block);
-            }
+            snake.DrawBody(window);
+            window.Draw(text);
             window.Draw(snake.head);
             window.Display();
 
@@ -72,37 +79,14 @@ class SnakeGame
             snake.direction = Snake.Direction.Right;
         }
     }
-    /*public static void GetInput(Snake snake)
-    {
-        if (gameOver)
-        {
-            return;
-        }
-        if (Keyboard.IsKeyPressed(Keyboard.Key.Up))
-        {
-            snake.direction = Snake.Direction.Up;
-        }
-        else if (Keyboard.IsKeyPressed(Keyboard.Key.Down))
-        {
-            snake.direction = Snake.Direction.Down;
-        }
-        else if (Keyboard.IsKeyPressed(Keyboard.Key.Left))
-        {
-            snake.direction = Snake.Direction.Left;
-        }
-        else if (Keyboard.IsKeyPressed(Keyboard.Key.Right))
-        {
-            snake.direction = Snake.Direction.Right;
-        }
-    }*/
     public static Vector2f GridToVector2f(Vector2f gridPos)
     {
-        return new Vector2f(gridPos.X * 16, gridPos.Y * 16);
+        return new Vector2f(gridPos.X * BLOCK_SIZE, gridPos.Y * BLOCK_SIZE);
     }
 }
 class Snake : ICollidable
 {
-    private const int HEAD_SIZE = 16;
+    private const int HEAD_SIZE = SnakeGame.BLOCK_SIZE;
     public event EventHandler<KeyEventArgs> KeyPressed = null;
     public enum Direction
     {
@@ -117,10 +101,13 @@ class Snake : ICollidable
     public RectangleShape head = new RectangleShape(new Vector2f(HEAD_SIZE, HEAD_SIZE));
     public Direction direction;
     public Vector2f Position { get { return head.Position; } set { head.Position = value; } }
+    private Stopwatch stopwatch = new Stopwatch();
+
     public Snake(Color color)
     {
         head.FillColor = color;
         direction = Direction.Right;
+        stopwatch.Restart();
     }
     public void Move()
     {
@@ -128,7 +115,7 @@ class Snake : ICollidable
         int x = (int)gridPos.X;
         int y = (int)gridPos.Y;
         switch (direction)
-        {          
+        {
             case Direction.Up:
                 if (y - 1 < 0 || grid[x, y - 1] != null)
                 {
@@ -145,7 +132,7 @@ class Snake : ICollidable
                 }
                 GridPos = new Vector2f(x, y + 1);
                 break;
-            case Direction.Left:         
+            case Direction.Left:
                 if (x - 1 < 0 || grid[x - 1, y] != null)
                 {
                     SnakeGame.gameOver = true;
@@ -169,20 +156,34 @@ class Snake : ICollidable
         }
         blocks.Add(block);
     }
+    public void RemoveBody()
+    {
+        if (blocks.Count == 1)
+            return;
+        blocks.RemoveAt(0);
+        SnakeGame.score++;
+    }
+
+    public void DrawBody(RenderWindow window)
+    {
+        foreach (Block block in blocks)
+        {
+            window.Draw(block.block);
+        }
+    }
 }
 class Block : ICollidable
 {
     public Vector2f gridPos = new Vector2f();
 
     public Vector2f GridPos { get { return gridPos; } set { gridPos = value; block.Position = SnakeGame.GridToVector2f(gridPos); } }
-    public RectangleShape block = new RectangleShape(new Vector2f(16, 16));
-    public Block(Vector2f gridPos ,Color color)
+    public RectangleShape block = new RectangleShape(new Vector2f(SnakeGame.BLOCK_SIZE, SnakeGame.BLOCK_SIZE));
+    public Block(Vector2f gridPos, Color color)
     {
         block.FillColor = color;
         GridPos = gridPos;
     }
 }
-
 interface ICollidable
 {
 }
